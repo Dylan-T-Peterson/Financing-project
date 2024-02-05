b7f6d850a45177dd3fca7ffe18a7e2e7be34b26d
Dylan-T-Peterson <dylantypeterson@gmail.com>
2024-02-05 12:15:55 -0900

    refactored current code (main = add_hours()rn)

--- daily.py
+++ daily.py
@@ -1,27 +1,27 @@
 #!./venv/bin/python3
 import sqlite3
-import datetime
+import datetime as dt
 
 import numpy as np
 import pandas as pd
-from pandas._libs.tslibs.parsing import DateParseError
 
 
-def yn_verify(data: dict):
+def dtype_verify(to_change: str, new_type: dt.date | dt.time) -> dt.date | dt.time:
     while True:
-        change = input(
-            f"{data}\nWhich would you like to change? keep empty to continue: "
-        )
-        change_type = type(data[change])
-        if change == "":
-            break
-        if change not in data.keys():
-            continue
         try:
-            data[change] = change_type(input(f"replace {change}: "))
-            print(change, change_type)
+            changed = new_type.fromisoformat(to_change)
+            return changed
+        except ValueError:
+            match new_type:
+                case dt.date:
+                    to_change = input("Please enter a valid date[YY-MM-DD]: ")
+                case dt.time:
+                    to_change = input(f"Invalid format, retry {x}[HH:MM]: ")
+                case _:
+                    print("Unsupported type for function dtype_verify")
+                    quit()
         except Exception as e:
-            print(e)
+            print(f"{e}\nHow did you get here?? Contact me with the error.")
             quit()
 
 
@@ -88,19 +88,18 @@ if __name__ == "__main__":
     # TODO: create jobs table in finances.db, modify init.sql
     # TDC: needed columns would probably be:
     #   start_date(primary?)-DATE, rate-FLOAT, pay_schedule-DATE, possibly more
-    rate = int
+    rate = float
     conn = db_init()
     i_data = {
-        "day": datetime.date,
-        "clock-in": pd.Timestamp,
-        "lunch-out": pd.Timestamp,
-        "lunch-in": pd.Timestamp,
-        "clock-out": pd.Timestamp,
+        "day": dt.date,
+        "clock-in": dt.time,
+        "lunch-out": dt.time,
+        "lunch-in": dt.time,
+        "clock-out": dt.time,
     }
     be_data = {
-        "worked": pd.Timedelta,
-        "check-cutoff": datetime.date.fromisoformat("20240125"),
-        "check-date": datetime.date.fromisoformat("20240216"),
+        "worked": dt.timedelta,
+        "check-date": dt.date,
         "expected-check": float,
     }
 
@@ -108,29 +107,26 @@ if __name__ == "__main__":
     # db datatypes are specified in init.sql, specifically hours table
     i_data["day"] = input("day[YYYY-MM-DD] (leave empty if today): ")
     if i_data["day"] == "":
-        i_data["day"] = datetime.date.today()
+        i_data["day"] = dt.date.today()
+    else:
+        dtype_verify(i_data["day"], dt.date)
     for x in list(i_data.keys())[1:]:
         i_data[x] = input(f"{x} [HH:MM]: ")
-        while True:
-            if x == "":
-                i_data[x] = input("Please enter a time [HH:MM]: ")
-                continue
-            try:
-                i_data[x] = pd.Timestamp(f"{i_data['day']}T{i_data[x]}")
-                break
-            except DateParseError as e:
-                print(type(e))
-                i_data[x] = input(f"Invalid format, retry {x}[HH:MM]: ")
-    while True:
-        for k, v in dict(list(i_data.items())[:5]).items():
-            print(f"{k}: {v}")
-        match input("does this look valid? y/n: "):
-            case "n":
-                yn_verify(i_data)
-            case "y":
-                break
-            case _:
-                continue
+        dtype_verify(i_data[x], dt.time)
+
+    for key, val in i_data.items():
+        print(f"{key}: {val}")
+    x = input("does this look valid? y/n: ")
+    while x != "y":
+        change = input(
+            f"{i_data}\nWhich would you like to change? keep empty to keep: "
+        )
+        if change == "":
+            break
+        if change not in i_data.keys():
+            continue
+        dtype_verify(change, type(i_data[change]))
+        x = input("does this look valid? y/n: ")
 
     # Start calculating be_data
     # worked is diff of punched hours
